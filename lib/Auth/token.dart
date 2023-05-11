8cd88739632b772af86236baa715b0205c455f3f
Eyob <eyobdesta562@gmail.com>
2023-05-11 12:23:39 +0300

    token page fun

---
+++ lib/Auth/token.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Token {
+  localSave(t) async {
+    debugPrint("Saving token");
+    final pref = await SharedPreferences.getInstance();
+    await pref.setString('Token', t.toString());
+    debugPrint("Data saved locally");
+  }
+}
